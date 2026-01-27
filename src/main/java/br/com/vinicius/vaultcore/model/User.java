579da0d7611df067bfe48a648a5afb021d4bf0d6
Vinicius Esmeraldo <vinicius.esmeraldodev@gmail.com>
2026-01-27 16:50:10 -0300

    Implementando modelos de User e Wallet com relacionamento OneToOne

--- src/main/java/br/com/vinicius/vaultcore/model/User.java
+++ src/main/java/br/com/vinicius/vaultcore/model/User.java
@@ -1,4 +1,47 @@
 package br.com.vinicius.vaultcore.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Entity
+@Table(name = "tb_users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String name;
+
+    @Column(unique = true)
+    @Email(message = "Email inválido")
+    @NotBlank
+    private String email;
+
+    @Column(unique = true)
+    @CPF(message = "CPF inválido")
+    @NotBlank
+    private String cpf;
+
+    @NotBlank
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Wallet wallet;
 }
--- src/main/java/br/com/vinicius/vaultcore/model/Wallet.java
+++ src/main/java/br/com/vinicius/vaultcore/model/Wallet.java
@@ -1,4 +1,39 @@
 package br.com.vinicius.vaultcore.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_wallets")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wallet {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+        user.setWallet(this);
+    }
 }
