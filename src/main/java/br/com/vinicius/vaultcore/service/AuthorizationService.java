2c40934edb360d37f54ac364128bf2843d1be80c
Vinicius Esmeraldo <vinicius.esmeraldodev@gmail.com>
2026-01-29 18:07:39 -0300

    implementado endpoint de login e geração de token JWT

--- src/main/java/br/com/vinicius/vaultcore/service/AuthorizationService.java
+++ src/main/java/br/com/vinicius/vaultcore/service/AuthorizationService.java
@@ -1,4 +1,23 @@
 package br.com.vinicius.vaultcore.service;
 
-public class AuthorizationService {
+import br.com.vinicius.vaultcore.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+    }
+
 }
